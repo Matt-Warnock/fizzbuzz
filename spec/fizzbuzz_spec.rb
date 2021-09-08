10ec7f57cc86f416dac8b07c058a7983a41f79bf
KNtanos <86077849+Kntanos@users.noreply.github.com>
2021-09-08 13:02:33 +0100

    written test for 3 and 5

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -5,7 +5,23 @@ describe "fizzbuzz" do
     expect(fizzbuzz(3)).to eq "fizz"
   end
 
+  it "returns 'fizz' when passed 6" do
+    expect(fizzbuzz(6)).to eq "fizz"
+  end
+
+  it "returns 'fizz' when passed 9" do
+    expect(fizzbuzz(9)).to eq "fizz"
+  end 
+
   it "returns 'buzz' when passed 5" do
     expect(fizzbuzz(5)).to eq "buzz"
+  end
+
+  it "returns 'buzz' when passed 10" do
+    expect(fizzbuzz(10)).to eq "buzz"
+  end
+
+  it "returns 'buzz' when passed 15" do
+    expect(fizzbuzz(15)).to eq "buzz"
   end
 end
